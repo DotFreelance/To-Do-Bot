f88760ff1f10e17107a283dc1ea5caf5c2d08acd
Paul Jarrow <paul.jarrow@umsats.ca>
2016-02-01 22:47:25 -0600

    Re-initialize Project

---
+++ app/src/main/java/uofm/software_engineering/group7/to_do_bot/TaskListActivity.java
@@ -0,0 +1,13 @@
+package uofm.software_engineering.group7.to_do_bot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+    }
+}
