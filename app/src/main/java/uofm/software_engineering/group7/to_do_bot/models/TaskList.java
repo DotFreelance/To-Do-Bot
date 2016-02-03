dfc3dd220a77bbf1bcc6f7e9c2299d2d9e43a87e
Faye Lim <fayelimm@gmail.com>
2016-02-02 23:52:03 -0600

    Added models

---
+++ app/src/main/java/uofm/software_engineering/group7/to_do_bot/models/TaskList.java
@@ -0,0 +1,18 @@
+package uofm.software_engineering.group7.to_do_bot.models;
+
+/**
+ * Created by Faye on 1/22/2016.
+ *
+ * Please refer to the following link for HashMap use:
+ * https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html
+ */
+
+
+import java.util.HashMap;
+
+import uofm.software_engineering.group7.to_do_bot.models.ListItem;
+
+
+public class TaskList extends HashMap<Integer, ListItem> {
+
+}
