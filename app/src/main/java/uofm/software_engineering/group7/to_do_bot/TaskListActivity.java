986174089e925a5973ded042a1841546b221d0ce
Faye Lim <fayelimm@gmail.com>
2016-02-17 15:47:53 -0600

    Added check

--- app/src/main/java/uofm/software_engineering/group7/to_do_bot/TaskListActivity.java
+++ app/src/main/java/uofm/software_engineering/group7/to_do_bot/TaskListActivity.java
@@ -1,12 +1,25 @@
 package uofm.software_engineering.group7.to_do_bot;
 
+import android.app.Activity;
+import java.util.ArrayList;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class TaskListActivity extends AppCompatActivity {
+
+public class TaskListActivity extends AppCompatActivity
+{
+    private ArrayList<String> arrayListToDo;
+    private ArrayAdapter<String> arrayAdapterToDo;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
 
