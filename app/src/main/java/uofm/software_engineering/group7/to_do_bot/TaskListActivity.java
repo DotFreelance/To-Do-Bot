32dccc6e83c776367e62cce1683e5ea8983b6396
doanv <doanv@myumanitoba.ca>
2016-02-17 16:53:36 -0600

    Cleaned up project and removed unnecessary files

--- app/src/main/java/uofm/software_engineering/group7/to_do_bot/TaskListActivity.java
+++ app/src/main/java/uofm/software_engineering/group7/to_do_bot/TaskListActivity.java
@@ -1,21 +1,11 @@
 package uofm.software_engineering.group7.to_do_bot;
 
-import android.app.Activity;
-import java.util.ArrayList;
-
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
 
 
 public class TaskListActivity extends AppCompatActivity
 {
-    private ArrayList<String> arrayListToDo;
-    private ArrayAdapter<String> arrayAdapterToDo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
