03f536517812e526caf8fe3fbf9d4b130730078b
Brzezinski <brzezinm@cs.umanitoba.ca>
2016-02-22 14:22:35 -0600

    Did unit testing, need to wait until rest of code is finished to complete

--- app/src/androidTest/java/uofm/software_engineering/group7/to_do_bot/AddItemTest.java
+++ app/src/androidTest/java/uofm/software_engineering/group7/to_do_bot/AddItemTest.java
@@ -1,6 +1,5 @@
 package uofm.software_engineering.group7.to_do_bot;
 
-import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -8,28 +7,21 @@ import junit.framework.TestCase;
 
 import uofm.software_engineering.group7.to_do_bot.models.TaskListManager;
 import uofm.software_engineering.group7.to_do_bot.services.TaskListContract;
-import uofm.software_engineering.group7.to_do_bot.services.TaskListDBHelper;
 
 /**
  * Created by Matt on 2016-02-22.
  */
 public class AddItemTest extends TestCase {
     private TaskListManager taskManager = new TaskListManager(null, "List");
-    private TaskListDBHelper taskListDB;
 
     public void addItemTest() {
         SQLiteDatabase db = taskManager.getTaskListDB().getReadableDatabase();
         taskManager.addTask(null, "Test");
-        ContentValues dbValues = new ContentValues();
         String countQuery = "SELECT count(*) FROM " + TaskListContract.TaskListItemSchema.COL_NAME_DESCRIPTION;
 
         Cursor cursor = db.rawQuery(countQuery, null);
         cursor.moveToFirst();
         int count = cursor.getInt(0);
-        if(count > 0) {
-            System.out.println("Success");
-        } else {
-            System.out.println("Failure");
-        }
+        assert(count > 0);
     }
 }
--- app/src/androidTest/java/uofm/software_engineering/group7/to_do_bot/ApplicationTest.java
+++ app/src/androidTest/java/uofm/software_engineering/group7/to_do_bot/ApplicationTest.java
@@ -1,27 +1,16 @@
 package uofm.software_engineering.group7.to_do_bot;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends TestCase {
+    private static AddItemTest addItemTest = new AddItemTest();
+    private static RemoveItemTest removeItemTest = new RemoveItemTest();
+
     public static void main(String[] args) {
-        TestCase test = new AddItemTest();
-        test.run();
-    }
-
-    public static Test suite() {
-        return null;
-    }
-
-    protected void setUp() {
-    }
-
-    protected void tearDown() {
-    }
-
-    public void removeItemTest() {
+        addItemTest.addItemTest();
+        removeItemTest.removeItemTest();
     }
 }
