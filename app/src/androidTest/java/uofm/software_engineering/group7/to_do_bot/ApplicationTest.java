a5472d7f09e8d7c7ac7002407a5e9bba830fc11b
Brzezinski <brzezinm@cs.umanitoba.ca>
2016-02-22 13:45:38 -0600

    Unit testing

--- app/src/androidTest/java/uofm/software_engineering/group7/to_do_bot/ApplicationTest.java
+++ app/src/androidTest/java/uofm/software_engineering/group7/to_do_bot/ApplicationTest.java
@@ -1,11 +1,14 @@
 package uofm.software_engineering.group7.to_do_bot;
 
 import junit.framework.TestCase;
+import uofm.software_engineering.group7.to_do_bot.models.TaskListManager;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends TestCase {
+    TaskListManager taskManager = new TaskListManager("List");
+
     protected void setUp() {
 
     }
@@ -15,7 +18,7 @@ public class ApplicationTest extends TestCase {
     }
 
     public void addItemTest() {
-
+        taskManager.addTask("New Task");
     }
 
     public void removeItemTest() {
